18b94a1380294091ad72cb12816dca6a14c372e9
DiezCano <jdiecan2926@gmail.com>
2026-04-09 16:38:12 +0200

    as

---
+++ src/main/java/EjercicioClase.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class EjercicioClase {
+
+    private LocalDateTime fechahora;
+    private double temperatura;
+    private double humedad;
+
+    public EjercicioClase(LocalDateTime fechahora, double temperatura, double humedad) {
+        this.fechahora = fechahora;
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+    }
+
+
+
+}
